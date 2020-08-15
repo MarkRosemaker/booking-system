15156c3cd976be66a4676a1b15079977b88e6ea9
MarkRosemaker <MarcoDRoesler@gmail.com>
2020-08-15 21:35:03 +0200

    implement bookings api

--- api/bookings/bookings.go
+++ api/bookings/bookings.go
@@ -2,35 +2,65 @@ package bookings
 
 import (
 	"net/http"
+
+	"github.com/MarkRosemaker/booking-system/courses"
+
+	"cloud.google.com/go/civil"
+	"github.com/MarkRosemaker/booking-system/course"
+	"github.com/MarkRosemaker/go-server/server/api"
+	"github.com/MarkRosemaker/go-server/server/context"
+	"github.com/MarkRosemaker/go-server/server/form"
 )
 
 func Respond(req *http.Request) interface{} {
-	// ctx, cancel := context.WithUserTimeout(req)
-	// defer cancel()
+	ctx, cancel := context.WithUserTimeout(req)
+	defer cancel()
 
-	// origin, err := point.FromRequest(req)
-	// if err != nil {
-	// 	return api.ErrBadRequest(err)
-	// }
+	var (
+		name string
+		date civil.Date
+		id   string
+		c    *course.Course
+		err  error
+	)
 
-	// var r int
-	// if r, err = radius.FromRequest(req); err != nil {
-	// 	return api.ErrBadRequest(err)
-	// }
+	// get all the user input
 
-	// success := make(chan bool)
-	// go func() <-chan bool {
-	// 	// nb = pts.Neighbors(*origin, r)
-	// 	success <- true
-	// 	return success
-	// }()
+	if name, err = form.GetStringE(req, "name"); err != nil {
+		return api.ErrBadRequest(err)
+	}
+
+	if date, err = form.GetDateE(req, "date"); err != nil {
+		return api.ErrBadRequest(err)
+	}
+
+	if id, err = form.GetStringE(req, "id"); err != nil {
+		return api.ErrBadRequest(err)
+	}
+
+	errChan := make(chan error)
+	go func() <-chan error {
+		// for now: just get from map (quick)
+		// later: get from database (potentially slow)
+
+		if c, err = courses.Get(id); err != nil {
+			errChan <- api.ErrBadRequest(err)
+			return errChan
+		}
+
+		errChan <- c.BookClass(name, date)
+		return errChan
+	}()
 
 	// timout if necessary
-	// select {
-	// case <-success:
-	// 	return nb
-	// case <-ctx.Done():
-	// 	return api.ErrWrap(ctx.Err())
-	// }
+	select {
+	case err = <-errChan:
+		if err != nil {
+			return api.ErrWrap(err)
+		}
+		return api.NewSuccessNow(0, "congratulations, you are now registered for the course '%s'", c.Name())
+	case <-ctx.Done():
+		return api.ErrWrap(ctx.Err())
+	}
 	return nil
 }
